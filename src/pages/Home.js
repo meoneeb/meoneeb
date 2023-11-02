54900f3eb5c9a67f59b7aec768e2a4e5a6dd1327
Oneeb Faisal <oneebfaisal@gmail.com>
2023-11-02 18:38:49 +0500

    stats

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,15 @@ import React from "react";
 import LandingPage from "../components/home/LandingPage";
 import Footer from "@/commonComponents/Footer";
 import AboutMe from "@/components/home/AboutMe";
+import Stats from "@/components/home/Stats";
 
 export default function HomeScreen() {
     return (
         <>
             <LandingPage />
             <AboutMe/>
-            <Footer/>          
+            <Stats/> 
+            <Footer/>         
         </>
     )
 }
