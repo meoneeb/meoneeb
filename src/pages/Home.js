61c4d6b40b1aad3f0698b8901c1899395b409b69
Oneeb Faisal <oneebfaisal@gmail.com>
2023-11-02 14:22:39 +0500

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,12 @@ import React from "react";
 import LandingPage from "../components/home/LandingPage";
 import Footer from "@/commonComponents/Footer";
 import AboutMe from "@/components/home/AboutMe";
-import Stats from "@/components/home/Stats";
 
 export default function HomeScreen() {
     return (
         <>
             <LandingPage />
             <AboutMe/>
-            {/* <Stats/> */}
             <Footer/>          
         </>
     )
