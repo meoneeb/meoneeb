f3a0c05f237a89969f41679cb74a644be50622cc
Oneeb Faisal <oneebfaisal@gmail.com>
2023-11-03 00:55:56 +0500

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 import Head from 'next/head'
 import Image from 'next/image'
-import HomeScreen from '@/pages/home'
+import HomeScreen from '@/pages/HomePage'
 import '../styles/globals.scss'
 
 export default function Home({ Component, pageProps }) {
