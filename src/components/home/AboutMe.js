ec97f9a56a92d8e7ebd6398c5c89758f4fb0f57b
Oneeb Faisal <oneebfaisal@gmail.com>
2023-11-02 14:13:54 +0500

    stats

--- src/components/home/AboutMe.js
+++ src/components/home/AboutMe.js
@@ -2,25 +2,27 @@ import React, { useEffect, useState} from "react";
 
 export default function AboutMe() {
 
-    const skills = ['UI Design', 'Graphic Design', 'Wireframing', 'Prototyping', 'Figma', 'Adobe XD'];
+    const skills = ['UI Design', 'Graphic Design', 'Wireframing', 'Prototyping', 'Design System', 'User-flow', 'Visual Design', 'Typography', 'Figma', 'Adobe XD', 'Adobe Photoshop', 'Adobe Illustrator', 'Adobe After Effects', 'Adobe Premiere Pro', 'HTML/CSS', 'Javascript'];
 
     return (
         <>
-            <div className="d-flex back">
-                <div>
-                    <h3>
-                        About me
-                    </h3>
-                    <p>
-
-                    </p>
-                </div>
-                <div className="d-flex flex-wrap" style={{width: '500px'}}>
-                    {skills.map((skill) => (
-                        <span key={skill.id} style={{ margin: '4px', padding: '10px', border: '2px solid #8FF000', borderRadius: '15px', height: 'fit-content', color: '#fff'}}>
-                            {skill}
-                        </span>
-                    ))}
+            <div className="back">
+                <div className="container d-flex flex-wrap">
+                    <div className="col-lg-6">
+                        <h3 style={{ color: '#8fff00'}}>
+                            About me
+                        </h3>
+                        <p>
+                            I bring with more than four years of experience in the design industry. I have collaborated with renowned scientific research companies such as Abiosciences and Iview Therapeutics based in the US, as well as worked with familiar brands such as Hardees, Pizza Online and Pizza House on several successful projects. My services encompass a wide range of design-related disciplines such as Graphic Design, UX/UI Design, and Web Development.
+                        </p>
+                    </div>
+                    <div className="d-flex flex-wrap col-lg-6 gap-2">
+                        {skills.map((skill) => (
+                            <span key={skill.id} style={{ margin: '4px', padding: '10px', border: '2px solid #8FF000', borderRadius: '15px', height: 'fit-content', color: '#fff'}}>
+                                {skill}
+                            </span>
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
--- src/components/home/LandingPage.js
+++ src/components/home/LandingPage.js
@@ -6,27 +6,27 @@ export default function LandingPage() {
     const {IsMob} = useDevice()
 
     return (
-        <>  
-            <div
-                className="d-flex flex-column gap-4 p-2 justify-content-center align-items-center gradient-body"
-                style={{
-                    height: '100vh',
-                    
-                }}
-            >
-                <h1 className="mt-20" style={{color: '#8FFF00'}}>Hello! I am Oneeb Faisal,</h1>
-                <div
-                    className="d-flex flex-row gap-4 align-items-center"
-                >
-                    <h3 style={{color: '#fff'}}>Graphic & UI Designer</h3>
-                    <button className="btn-sm">About me</button>
-                </div>
-                <p style={{color: '#fff'}}>specialized in creating bespoke websites, digital products, and visual stories for fast-growing companies.</p>
-                <div
-                    className="d-flex py-4 flex-wrap gap-4"
-                >
-                    <button className="btn-lg">Hire me for a project</button>
-                    <button className="btn-lg-sec">See Portfolio ➞</button>
+        <>  <div className="gradient-body">
+                <div className="container fluid">
+                    <div
+                    className="d-flex flex-column gap-2 p-2 justify-content-center align-items-center gradient-body"
+                    style={{ height: '100vh'}}
+                    >
+                    <h1 className="mt-20" style={{color: '#8FFF00'}}>Hello! I am Oneeb Faisal,</h1>
+                    <div
+                        className="d-flex flex-row gap-4 align-items-center"
+                    >
+                        <h3 style={{color: '#fff'}}>Graphic & UI Designer</h3>
+                        <button className="btn-sm">About me</button>
+                    </div>
+                    <p style={{color: '#fff'}}>specialized in creating bespoke websites, digital products, and visual stories for fast-growing companies.</p>
+                    <div
+                        className="d-flex py-4 flex-wrap gap-4"
+                    >
+                        <button className="btn-lg">Hire me for a project</button>
+                        <button className="btn-lg-sec">See Portfolio ➞</button>
+                    </div>
+                    </div>
                 </div>
             </div>
         </>
